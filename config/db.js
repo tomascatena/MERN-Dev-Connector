334110830b74ce1ba0ee03a960eb468534637d8f
Tomás <tomascatena@gmail.com>
2021-03-24 21:50:52 +0100

    Define routes

--- config/db.js
+++ config/db.js
@@ -2,15 +2,19 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
+if (process.env.NODE_ENV !== 'production') {
+  const colors = require('colors');
+}
+
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('MongoDB Connected...');
+    console.log('MongoDB Connected...'.brightYellow.underline);
   } catch (error) {
-    console.error(error.message);
+    console.error(`${error.message}`.red.bold);
     // Exit process with failure
     process.exit(1);
   }
--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const connectDB = require('./config/db');
 
+if (process.env.NODE_ENV !== 'production') {
+  const colors = require('colors');
+}
+
 const app = express();
 
 // Connect to MongoDB
 connectDB();
 
-app.get('/', (req, res) => res.send('API Running'));
+app.get('/', (req, res) => res.send('API Running'.green));
+
+// Define Routes
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`.blue.bold));
