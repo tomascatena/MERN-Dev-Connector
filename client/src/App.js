bba09a2ea817be463c9f8d3531cbad608aac6442
Tomás <tomascatena@gmail.com>
2021-03-25 17:49:49 +0100

    Add Login and Register componetns, create Links in Landing and Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+// Components
 import Landing from './components/layout/Landing';
 import Navbar from './components/layout/Navbar';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 const App = () => {
   return (
-    <React.Fragment>
-      <Navbar />
-      <Landing />
-    </React.Fragment>
+    <Router>
+      <React.Fragment>
+        <Navbar />
+        <Route exact path="/" component={Landing} />
+        <section className="container">
+          <Switch>
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+          </Switch>
+        </section>
+      </React.Fragment>
+    </Router>
   );
 };
 
